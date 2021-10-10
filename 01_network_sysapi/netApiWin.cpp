8954888027e2e9ddcc27f4678e64309e69b7906b
Andrii Baryshnychenko <andreyversa@gmail.com>
2021-10-10 22:59:54 +0300

    write function tcp_server for create listen socket
    Signed-off-by: Andrii Baryshnychenko <andreyversa@gmail.com>

--- 01_network_sysapi/netApiWin.cpp
+++ 01_network_sysapi/netApiWin.cpp
@@ -2,13 +2,51 @@
 #define WINNETAPI_HPP
 
 #include <cstdio>
+#include <cstring>
 
 #include "netApi.hpp"
 
 namespace network {
 	
 	SOCKET tcp_server(char *hostname, char *servname) {
-		return 8400;
+		struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+		
+		int status = getaddrinfo(hostname, servname, &hints, &result);
+		if (status != 0) {
+			printf("Error at getaddrinfo(): %ld\n", WSAGetLastError());
+			return INVALID_SOCKET;
+		}
+		
+		ptr=result;
+		
+		SOCKET ListenSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (ListenSocket == INVALID_SOCKET) {
+			printf("Error at socket(): %ld\n", WSAGetLastError());
+			freeaddrinfo(result);
+			return INVALID_SOCKET;
+		}
+		
+		status = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+		if (status == SOCKET_ERROR) {
+			printf("bind failed with error: %d\n", WSAGetLastError());
+			freeaddrinfo(result);
+			closesocket(ListenSocket);
+			return INVALID_SOCKET;
+		}
+		freeaddrinfo(result);
+		
+		if ( listen( ListenSocket, SOMAXCONN ) == SOCKET_ERROR ) {
+			printf( "Listen failed with error: %ld\n", WSAGetLastError() );
+			closesocket(ListenSocket);
+			return INVALID_SOCKET;
+		}
+		
+		return ListenSocket;
 	}
 	
 }
