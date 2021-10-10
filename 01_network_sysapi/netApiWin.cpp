9b1fc288afd103755f7f8b035ffaeabcd3c80b73
Andrii Baryshnychenko <andreyversa@gmail.com>
2021-10-10 22:53:12 +0300

    create first cmake build project for client, server and network library. Add configure a header file PlatformConfig.hpp.in for define platform dependent difinitions. Write simple functions tcp_server in network library for test cmake build. Write INIT() macros for initialize winsock.
    Signed-off-by: Andrii Baryshnychenko <andreyversa@gmail.com>

---
+++ 01_network_sysapi/netApiWin.cpp
@@ -0,0 +1,16 @@
+#ifndef WINNETAPI_HPP
+#define WINNETAPI_HPP
+
+#include <cstdio>
+
+#include "netApi.hpp"
+
+namespace network {
+	
+	SOCKET tcp_server(char *hostname, char *servname) {
+		return 8400;
+	}
+	
+}
+
+#endif // WINNETAPI_HPP
