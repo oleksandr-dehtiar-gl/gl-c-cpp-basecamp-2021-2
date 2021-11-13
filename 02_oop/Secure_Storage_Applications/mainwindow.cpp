721460774c203abc1dea0912d98a9e0bf70e4f77
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-11-13 17:36:05 +0200

    Buttons refactoring

--- 02_oop/Secure_Storage_Applications/mainwindow.cpp
+++ 02_oop/Secure_Storage_Applications/mainwindow.cpp
@@ -4,7 +4,8 @@
 #include <QtXml>
 #include <QTextStream>
 #include <QXmlStreamReader>
-
+#include <QXmlStreamWriter>
+#include <algorithm>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -17,6 +18,24 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QString MainWindow::fileName(QString filePath)
+{
+    QString fileName;
+
+    for (int i = filePath.size() - 1; ; i--)
+    {
+        if(filePath[i] != '/')
+        {
+            fileName.append(filePath[i]);
+        }
+        else if(filePath[i] == '/')
+        {
+            break;
+        }
+    }
+    std::reverse(fileName.begin(), fileName.end());
+    return fileName;
+}
 
 void MainWindow::on_pushButton_2_clicked()
 {
@@ -31,48 +50,95 @@ void MainWindow::on_pushButton_clicked()
     this->storagePath = QFileDialog::getOpenFileName(this, "Open Storage", "C://", "XML (*.xml)");
 }
 
+std::map<QString, QString> MainWindow::readStorage()
+{
+    QFile xmlFile(storagePath);
+    if (!xmlFile.open(QFile::ReadOnly | QFile::Text ))
+    {
+        // Error while loading file
+    }
+
+    QXmlStreamReader xmlReader;
+    xmlReader.setDevice(&xmlFile);
+    xmlReader.readNext();   // Переходит к первому элементу в файле
+
+    QString tempString;
+    QString fileName;
+    QString fileContent;
+    std::map<QString, QString> xmlContent;
+    while(!xmlReader.atEnd())
+    {
+        if(xmlReader.isStartElement())
+        {
+            if(xmlReader.name() == "File")
+            {
+                foreach(const QXmlStreamAttribute &attr, xmlReader.attributes())
+                {
+                    fileName = attr.value().toString();
+                }
+                fileContent = xmlReader.readElementText();
+                xmlContent.emplace(fileName, fileContent);
+            }
+
+        }
+
+
+        xmlReader.readNext();
+    }
+    xmlFile.close();
+    return xmlContent;
+}
+
 void MainWindow::on_pushButton_3_clicked()
 {
+    std::map<QString, QString> xmlContent;
+    xmlContent = readStorage();
     QString newFilePath = QFileDialog::getOpenFileName(this, "Add file", "C://", "All (*.*)");
+    QString newFileName = fileName(newFilePath);
     QFile newFile(newFilePath);
     if(!newFile.open(QFile::ReadOnly))
     {
         qDebug() << "Already opened or there is another issue";
         newFile.close();
     }
+
     QTextStream newFileContent(&newFile);
-
-
+    newFileContent.setCodec("UTF-8");
+    QString tempString = newFileContent.readAll();
 
     QFile xmlFile(storagePath);
-    if (!xmlFile.open(QFile::WriteOnly | QFile::Text ))
+    xmlFile.open(QIODevice::WriteOnly);
+
+    QXmlStreamWriter xmlWriter(&xmlFile);
+    xmlWriter.setAutoFormatting(true);  // Устанавливаем автоформатирование текста
+    xmlWriter.writeStartDocument();     // Запускаем запись в документ
+    xmlWriter.writeStartElement("Files");   // Записываем первый элемент с его именем
+    for (const auto& i : xmlContent)
     {
-       qDebug() << "Already opened or there is another issue";
-       xmlFile.close();
+        xmlWriter.writeStartElement("File");
+        xmlWriter.writeAttribute("name",i.first);
+        xmlWriter.writeCharacters(i.second);
+        xmlWriter.writeEndElement();
     }
 
-    QTextStream xmlContent(&xmlFile);
+    xmlWriter.writeStartElement("File");  // Записываем тег с именем для первого чекбокса
+    xmlWriter.writeAttribute("name", newFileName);
 
-    QDomDocument document;
-
-    QDomElement root = document.createElement("Files");
-    document.appendChild(root);
-
-    QDomElement student = document.createElement("File");
-    student.setAttribute("Number", "1");
-
-    student.appendChild(document.createTextNode(newFileContent.readAll()));
-    root.appendChild(student);
-
-    xmlContent << document.toString();
-    xmlFile.close();
-
+    xmlWriter.writeCharacters(tempString);
+    xmlWriter.writeEndElement();        // Закрываем тег
+    /* Закрываем тег "Files"
+     * */
+    xmlWriter.writeEndElement();
+    /* Завершаем запись в документ
+     * */
+    xmlWriter.writeEndDocument();
+    newFile.close();
+    xmlFile.close();   // Закрываем файл
 }
 
 
 void MainWindow::on_pushButton_GetObject_clicked()
 {
-    //QString tempPath = QFileDialog::getOpenFileName(this, "Open Storage", "C://", "XML (*.xml)");
     QFile xmlFile(storagePath);
     if (!xmlFile.open(QFile::ReadOnly | QFile::Text ))
     {
@@ -90,14 +156,21 @@ void MainWindow::on_pushButton_GetObject_clicked()
         {
             if(xmlReader.name() == "File")
             {
-                tempString = xmlReader.readElementText();
+                foreach(const QXmlStreamAttribute &attr, xmlReader.attributes())
+                {
+                    if(ui->FileName->text() == attr.value().toString())
+                    {
+                        tempString = xmlReader.readElementText();
+                        break;
+                    }
+                }
             }
+
         }
 
 
         xmlReader.readNext();
     }
-
 
 
     QString fileName = QFileDialog::getSaveFileName(this, "Save file", "C://", "txt (*.txt)");
---
+++ 02_oop/build-Secure_Storage_Applications-Desktop_Qt_5_15_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,127 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_GetObject;
+    QPushButton *pushButton_4;
+    QSpacerItem *verticalSpacer;
+    QLineEdit *FileName;
+    QSpacerItem *horizontalSpacer;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_GetObject = new QPushButton(centralwidget);
+        pushButton_GetObject->setObjectName(QString::fromUtf8("pushButton_GetObject"));
+
+        verticalLayout->addWidget(pushButton_GetObject);
+
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        verticalLayout->addWidget(pushButton_4);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        FileName = new QLineEdit(centralwidget);
+        FileName->setObjectName(QString::fromUtf8("FileName"));
+
+        horizontalLayout->addWidget(FileName);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 20));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Create new storage", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Open storage", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Add new object", nullptr));
+        pushButton_GetObject->setText(QCoreApplication::translate("MainWindow", "Get Object", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Delete Object", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
